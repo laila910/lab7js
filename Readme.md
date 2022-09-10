a7cac39058441ae95bace08a5953482a2e14dd7e
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2022-09-10 23:37:57 +0200

    someedits

--- Readme.md
+++ Readme.md
@@ -6,9 +6,29 @@
  like the following example:
  !['Example image'](img/example1.jpg)
 
+# Solve Lap1
+> 1. image slider as:
+  ![image](Slove/imageslider.jpg)
+
+ 2. After click next as:
+ ![image](Slove/afterclickNext.jpg)
+
+ 3. after click back:
+ ![image](Slove/afterclickback.jpg)
+
 # lap2
 > Lap2
  Create Digital watch like the following with alarm option ( you can use alert dialog
  box to alarm the user instead of sound) 
  like the following example:
  !['example image 2'](img/example2.jpg)
+
+# Solve Lap2
+> 1. current Time as:
+ ![image](Slove/CurrentTime.jpg)
+
+ 2. set alarm as:
+ ![image](Slove/setAlarm.jpg)
+
+ 3. alert when current Time equal to alarm Time as:
+ ![image](Slove/alertwhentimeMatchwithAlarm.jpg)
--- fun.js
+++ fun.js
@@ -112,7 +112,7 @@ function alarmClear() {
 	document.getElementById('ampm').disabled = false;
 	sound.pause();
 }
-
+// -----------------------------slider
 var slideIndex = 1;
 showSlides(slideIndex);
 function plusSlides(n) {
@@ -125,13 +125,18 @@ function currentSlide(n) {
          var i;
          var slides = document.getElementsByClassName("mySlides");
          var dots = document.getElementsByClassName("dot");
-         if (n > slides.length) {slideIndex = 1}
-         if (n < 1) {slideIndex = slides.length}
+         if (n > slides.length) {
+            slideIndex = 1;
+        }
+         if (n < 1) {
+            slideIndex = slides.length;
+        }
          for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
+            
          }
          for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
+            dots[i].className = dots[i].className.replace("active", "");
          }
          slides[slideIndex-1].style.display = "block";
          dots[slideIndex-1].className += " active";
