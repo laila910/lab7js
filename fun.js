c76d1e2ef1d68f68cf68db5147b3bd8e752a35ad
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2022-09-10 12:55:42 +0200

    finish alarm

--- fun.js
+++ fun.js
@@ -1,109 +1,109 @@
-(function Clock() {
-    var date = new Date();
-    var hour = date.getHours();
-    // console.log(hour);
-    var minute = date.getMinutes();
-    // console.log(minute);
-    var second = date.getSeconds();
-    // console.log(second);
-    var Day=date.getDay();//Sunday = 0, Monday = 1, Tuesday = 2, wednesday = 3 , thursday = 4 , friday = 5, saturday = 6
-    switch(Day){
-        case 0:
-            console.log('Sunday');
-            Day='Sun';
-            break;
-        case 1:
-            console.log('Monday');
-            Day='Mon';
-            break;
-        case 2:
-            console.log('tuesday');
-            Day='Tue';
-            break;
-        case 3:
-            console.log('wednesday');
-            Day='Wed';
-            break;
-        case 4:
-            console.log('thursday');
-            Day='Thu';
-            break;
-        case 5:
-            console.log('friday');
-            Day='Fri';
-            break;
-        case 6:
-            console.log('saturday');
-            Day='Sat';
-            break;
+var currentTime = setInterval(function(){
+	var date = new Date();
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var seconds = date.getSeconds();
+	var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
+	//convert military time to standard time
+if(hours <10){
+    hours='0' +hours;
+}else{
+    hours=hours;
+}
+if(minutes < 10){
+    minutes='0'+minutes;
+}else{
+    minutes=minutes;
+}
+if(seconds <10){
+    seconds='0'+seconds;
+}else{
+    seconds=seconds;
+}
 
-    }
-    // console.log(Day);
-    var period = "";
-    if (hour >= 12) {
-    period = "PM";
-    }else{
-    period = "AM";
-    }
-    // Converting the hour in 12-hour format
-    if (hour == 0) {
-    hour = 12;
-    } else {
-    if (hour > 12) {
-    hour = hour - 12;
-    }
-    } 
-    //Updating hour, minute, and second if they are less than 10
-    if (hour < 10) {
-        hour="0" + hour;
-    }
-    if (minute < 10) {
-        minute= "0" + minute;
-    }
-       
-    if (second < 10) {
-        second="0" + second;
-    }
-    // Adding time elements to the div
-    document.getElementById("Clock").innerText = Day+'->' + hour + " : " + minute + " : " + second + " " + period;
-    // Set Timer to 1 sec (1000 ms)
-    setTimeout(Clock, 1000);
-   }());
-//----------------------------------------------------------------------------------------------------------
+	document.getElementById('clock').textContent = hours + ":" + minutes + ":" + seconds + " " + ampm;
+	
+},1000);
 // Alarm
-var alarm = null;
-(function setAlarm(event){
-    event.preventDefault();
-   // console.log(event.target[0].value);
-    var hr=event.target[0].value;
-    var min=event.target[1].value;
-    var sec=event.target[2].value;
-    var zone=event.target[3].value;
- 
-    // console.log(hour , min , sec,zone);
- //Updating hour, minute, and second if they are less than 10
- if (hr < 10) {
-    hr="0" + hr;
-}
-if (min < 10) {
-    min= "0" + min;
-}
+function alarmSet() {
+
+	var hr = document.getElementById('alarmhrs');
+	
+	var min = document.getElementById('alarmmins');
+	
+	var sec = document.getElementById('alarmsecs');
+	
+	var ap = document.getElementById('ampm');
+    
+
+    var selectedHour = hr.options[hr.selectedIndex].value;
+    var selectedMin = min.options[min.selectedIndex].value;
+    var selectedSec = sec.options[sec.selectedIndex].value;
+    var selectedAP = ap.options[ap.selectedIndex].value;
+    
+
+    var alarmTime = selectedHour + ":" + selectedMin + ":" + selectedSec + selectedAP;
+    console.log('alarmTime:' + alarmTime);
+
+    document.getElementById('alarmhrs').disabled = true;
+	document.getElementById('alarmmins').disabled = true;
+	document.getElementById('alarmsecs').disabled = true;
+	document.getElementById('ampm').disabled = true;
+
+//when alarmtime is equal to currenttime then play a sound
+setInterval(function(){
+
+	var date = new Date();
+	
+	var hours = date.getHours();
+	
+	var minutes = date.getMinutes();
+	
+	var seconds = date.getSeconds();
+	
+	var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
+
+
+	//convert military time to standard time
+
+	if(hours <10){
+        hours='0' +hours;
+    }else{
+        hours=hours;
+    }
+    if(minutes < 10){
+        minutes='0'+minutes;
+    }else{
+        minutes=minutes;
+    }
+    if(seconds <10){
+        seconds='0'+seconds;
+    }else{
+        seconds=seconds;
+    }
    
-if (sec < 10) {
-    sec="0" + sec;
+	
+	var currentTime = document.getElementById('clock').textContent = hours+ ":" + minutes + ":" + seconds+ "" + ampm;
+	
+console.log(currentTime);
+	if (alarmTime == currentTime) {
+		alert('wake up,alarm'.alarmTime);
+		}
+
+},1000);
+
+
+	 console.log('currentTime:' + currentTime);	
+
 }
-// document.getElementById("alarm").innerText =  hr + " : " + min + " : " + sec + " " + zone;
 
-//   alarm = getTimeString({
-//     hours: hour.value,
-//     seconds: sec.value,
-//     minutes: min.value,
-//     zone: zone.value
-//   });
-//   Reset form after submit
-  document.forms[0].reset();
-}())
 
-// Attach submit event to the form
-document.forms[0].addEventListener("submit", setAlarm);
+function alarmClear() {
+
+	document.getElementById('alarmhrs').disabled = false;
+	document.getElementById('alarmmins').disabled = false;
+	document.getElementById('alarmsecs').disabled = false;
+	document.getElementById('ampm').disabled = false;
+	sound.pause();
+}
 
